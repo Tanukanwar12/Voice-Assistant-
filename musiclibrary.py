fe1a3ee2676f4a8ae23968fe3bc8e9b2762a3459
Tanukanwar12 <bhatitanu374@gmail.com>
2025-10-09 00:54:51 +0530

    Create musiclibrary.py

---
+++ musiclibrary.py
@@ -0,0 +1,8 @@
+music={   
+    "beleiver":"https://www.youtube.com/watch?v=7wtfhZwyrcc&list=RD7wtfhZwyrcc&start_radio=1",
+    "golden":"https://www.youtube.com/watch?v=yebNIHKAC4A&list=RDyebNIHKAC4A&start_radio=1",
+    "dream":"https://www.youtube.com/watch?v=FMX98ROVRCE&list=RDFMX98ROVRCE&start_radio=1",
+    "jump":"https://www.youtube.com/watch?v=CgCVZdcKcqY&list=RDCgCVZdcKcqY&start_radio=1",
+                                                                                                                                                                                                                                                    
+
+}
